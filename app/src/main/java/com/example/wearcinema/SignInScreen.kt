b147e22022dd528cc793e0beaf030d12835c7c06
mad-user04 <adtspb.mad.user04@gmail.com>
2022-02-09 16:34:53 +0300

    Initial commit

---
+++ app/src/main/java/com/example/wearcinema/SignInScreen.kt
@@ -0,0 +1,22 @@
+package com.example.wearcinema
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_sign_in_screen.*
+
+class SignInScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in_screen)
+
+    }
+
+
+}
+
+
